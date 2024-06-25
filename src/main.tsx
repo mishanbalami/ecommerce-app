d24b002170cdc9d71de941c2e6e039deb68cdf1b
mishanbalami <mishanbalami101@gmail.com>
2024-06-25 06:40:03 +0545

    git commited

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./app/products/products.tsx";
+import ProductDetail from "./app/products/product-detail.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/product-detail/:productId",
+    element: <ProductDetail />,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
